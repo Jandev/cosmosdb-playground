874dcdccc2d50e876d4ffee446e02253ef52e70f
Jan de Vries <jan@jan-v.nl>
2020-11-17 11:29:11 +0100

    Using nodemon for testing

--- src/CosmosDbApp/js/spCreateReservations.js
+++ src/CosmosDbApp/js/spCreateReservations.js
@@ -1,6 +1,20 @@
-﻿function helloworld() {
+﻿//@ts-check
+function helloworld() {
     var context = getContext();
     var response = context.getResponse();
-
+    console.log('hello');
     response.setBody("Hello, World");
+}
+
+helloworld();
+
+function getContext() {
+    return {
+        getResponse(){
+            return { 
+                setBody(){
+                }
+            }
+        } 
+    };
 }
---
+++ src/CosmosDbApp/js/spCreateReservations.spec.js
@@ -0,0 +1,9 @@
+﻿describe('test-runner'), () => {
+    it('should work fine'), () => {
+        const given = "works";
+
+        const when = given.charAt(0);
+
+        expect(when).toEqual("w");
+    }
+}
