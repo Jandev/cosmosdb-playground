eaf281b9d74a1184f5da213406d1d3e177fbd54c
Jan de Vries <jan@jan-v.nl>
2020-11-17 21:30:58 +0100

    Used stored procedure

--- src/CosmosDbApp/js/spCreateReservations.spec.js
+++
@@ -1,9 +0,0 @@
-﻿describe('test-runner'), () => {
-    it('should work fine'), () => {
-        const given = "works";
-
-        const when = given.charAt(0);
-
-        expect(when).toEqual("w");
-    }
-}
